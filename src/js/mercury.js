64d4f62a0632d87e61a8e5dcb7d4a869a76a3d86
Ultra Zacky <ultrazacky@gmail.com>
2022-02-04 10:59:33 -0800

    add test for mercuryAge bad fail

--- src/js/mercury.js
+++ src/js/mercury.js
@@ -4,4 +4,10 @@ export default class Mercury {
     this.age = age;
     this.lifeExpectancy = lifeExpectancy;
   }
+  mercuryAge() {
+    let earthAge = parseInt(mercury.age);
+    let mercuryDays = (age * 365);
+    let mercuryAge = (multAge / 88);
+    return ["Hello " + mercury.name + " your age on mercury is " + mercuryAge];
+  }
 }
