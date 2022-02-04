ef734cb4508e66333f7d5849f4a874e3ad514ef1
Ultra Zacky <ultrazacky@gmail.com>
2022-02-04 09:03:05 -0800

    test should return good fail on mercury constructor

--- __tests__/mercury.test.js
+++ __tests__/mercury.test.js
@@ -1,10 +1,8 @@
-import { TestScheduler } from 'jest';
 import Mercury from './../src/js/mercury.js';
 
 describe('Mercury', () => {
   test('should create new mercury object with name and age', () => {
     const mercury = new Mercury("Bob", '22');
-    expect(mercury.name).toEqual("Bob");
-    expect(mercury.age).toEqual("22");
+    expect(mercury).toEqual({"name":"Bob", "age": "22"});
   });
 });
