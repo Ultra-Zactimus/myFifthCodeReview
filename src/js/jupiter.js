e14caa26047750b3dc1e68d3504ac8f32620bb1e
Ultra Zacky <ultrazacky@gmail.com>
2022-02-04 15:38:16 -0800

    add code for timeParadox(), test passes

--- src/js/jupiter.js
+++ src/js/jupiter.js
@@ -4,5 +4,8 @@ export default function Jupiter(name, age, lifeExpectancy) {
   this.lifeExpectancy = lifeExpectancy;
 }
 Jupiter.prototype.timeParadox = function () {
-
+  let earthAge = parseInt(this.age);
+  let inJupiterDays = (earthAge * 365);
+  let whatIsExistence = Math.floor(inJupiterDays / 4380).toString();
+  return whatIsExistence;
 };
