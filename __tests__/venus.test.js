569e1843ce74e663d8cc89403ec3f9d0040f5faf
Ultra Zacky <ultrazacky@gmail.com>
2022-02-04 09:16:55 -0800

    test for good fail on venus constructor

--- __tests__/venus.test.js
+++ __tests__/venus.test.js
@@ -3,6 +3,6 @@ import Venus from './../src/js/venus.js';
 describe('Venus', ()=> {
 test('it should create a venus object with name and age', () => {
   const venus = new Venus("Suzy", "43");
-  expect(venus).toEqual("name": "Suzy", "age": "43");
+  expect(venus).toEqual({"name": "Suzy", "age": "43"});
 }); 
 });
