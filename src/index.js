c866a5c0c05a5c9c8cd207bf0169a21e560bc413
Ultra Zacky <ultrazacky@gmail.com>
2022-02-04 16:17:44 -0800

    add imports of Jupiter, Mars, Venus and Mercury to index.js

--- src/index.js
+++ src/index.js
@@ -2,3 +2,7 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import {Jupiter} from './js/jupiter.js';
+import {Mars} from './js/mars.js'; 
+import {Venus} from './js/venus.js'; 
+import {Mercury} from './js/mercury.js'; 
